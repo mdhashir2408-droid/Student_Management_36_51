2ebb7cf385fa73f6f405f12ae40efa71e09848ab
mdhashir2408-droid <mdhashir2408@gmail.com>
2026-06-19 16:52:47 +0530

    Create User model schema

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
@@ -31,4 +32,20 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Encrypt password using bcrypt before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 module.exports = mongoose.model('User', userSchema);
+
